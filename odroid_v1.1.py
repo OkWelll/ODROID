51177ae9bb4d131c96aba22f446eef71a75e939e
maks1798 <30872307+maks1798@users.noreply.github.com>
2017-08-09 19:29:48 +0300

    Update odroid_v1.1.py

--- odroid_v1.1.py
+++ odroid_v1.1.py
@@ -1,15 +1,10 @@
 import serial
 
-file = open("text.txt", "w")
-file.write('Beginning of file')
-file.close()
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 t = 0
 while True:
     if t < 10:
         file = open("text.txt", "a")
-        print(str(ser.readline()) + '\n')
-        file.write('str № ' + str(t))
         file.write(str(ser.readline()) + '\n')
         t += 1
         file.close()
